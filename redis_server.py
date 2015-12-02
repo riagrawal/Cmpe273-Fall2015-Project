ada89d5c657710fde52b448bb98aec5f3f42029e
Richa Agrawal <riagrawal18@gmail.com>
2015-12-02 03:18:08 -0800

    addition of hashring code and updated redis_server

--- redis_server.py
+++ redis_server.py
@@ -1,7 +1,18 @@
 import redis
-import xlrd
 import sys
+import hashing
+import hashring
 
+class ServerNode(object):
+    """Represents a Server node."""
+    
+    def __init__(self, port):
+        "Creates a new instance"
+        self._port = port
+    
+    def getPort(self):
+        "Returns the server port"
+        return self._port
 
 def getVariable(variable_name):
     my_server = redis.Redis(connection_pool=POOL)
@@ -16,7 +27,6 @@ def select_cluster(path,num_of_cluster,key):
     workbook = xlrd.open_workbook(path)
     address = [] 
     hash = int(key) % int(num_of_cluster)
-    print "Hash value for given input is ,", hash
     print "\n The cluster selected is : ", hash+1
     for sheet in workbook.sheets():
         for row in range(sheet.nrows):
@@ -36,22 +46,25 @@ def success(ip):
 	print "\nThe connection to Redis Server at port " + str(ip) +" established \n"
 
 if __name__ == "__main__":
-	path = "C:\Users\Rii\Documents\cmpe 273\project\\test.xlsx"
-	num_of_cluster =  countOfCluster(path)
-	opt = "y"
-	while(opt == "y"):
-		key = raw_input("Enter the value of key : ")
-		value = raw_input("Enter the value to be entered : ")
-		ip_address = select_cluster(path,num_of_cluster,key)
-		#try:
-		POOL = redis.ConnectionPool(host=ip_address[0], port=ip_address[1], db=0)
-		success(int(ip_address[1]))
-		#except Exception as e:
-		#	print "Connection to " +ip_address[0]+" not established!!!"
-		setVariable(key,value)
-		response = getVariable(key)
-		print "value of key - " + key + " -  is " + response
-		opt= raw_input("\nDo you want to continue ? y/n \n")
-	
+    ring = hashring.ConsitentHashing()
+    for i in range(100):
+        ring.add_node(6379+i*10)
+
+    ring.add_node(6380)
+    ring.add_node(7212)
+    ring.add_node(8020)
+
+    print " Scenario 1 "
+
+    print " HashRing for key=12341234 - hash=", ring.get_key("12341234") , "server=" ,ring.get_node("12341234")
+    print " HashRing for key=crap - hash=", ring.get_key("crap") ,  ring.get_node("crap")
+    print " HashRing for ajsvlaskd : ", ring.get_key("ajsvlaskd") ,  ring.get_node("ajsvlaskd")
+    print " HashRing for earth : ", ring.get_key("earth") ,  ring.get_node("earth")
+    print " HashRing for crap : ", ring.get_key("crap") ,  ring.get_node("crap")
+
+
+    
+
+
 	
 	
