1409e189f16a285222b20270e0f13d9e0c3be3b1
Richa Agrawal <riagrawal18@gmail.com>
2015-12-02 01:14:28 -0800

    removal of irrelevant code

--- Redis_273/src/hashing/HashFunction.java
+++
@@ -1,3 +0,0 @@
-public interface HashFunction {
-  int hash(Object s);
-}
--- Redis_273/src/hashing/MoveHandler.java
+++
@@ -1,6 +0,0 @@
-import java.util.List;
-
-public interface MoveHandler<K, V> {
-  public void moved(K from, K to, List<V> values);
-  public void rebuild(K key, List<V> allVals);
-}
